f458650de820ecc0dd604fe5f5fee6d45212a44c
Jonathan Deamer <202770+jonathandeamer@users.noreply.github.com>
2026-06-08 19:29:42 +0100

    Update yeti to v0.1.2

--- Formula/yeti.rb
+++ Formula/yeti.rb
@@ -1,8 +1,8 @@
 class Yeti < Formula
   desc "Ski down a mountain until the yeti finds you"
   homepage "https://github.com/jonathandeamer/yeti"
-  url "https://github.com/jonathandeamer/yeti/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "510df2b16367ae37915aaf77e9e213960f9abde39200939e5c34ef34ee423095"
+  url "https://github.com/jonathandeamer/yeti/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "54e415ba2701d217e26d0030416f5358f0b0cfcc3c4e150dfa1aa9c79689a2b8"
   license any_of: ["MIT", "CC0-1.0"]
 
   depends_on "ncurses"
